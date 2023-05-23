f7caea25187c8a5c51051735c7dd6564ec409fcc
Juanp-Gomez <131492820+Juanp-Gomez@users.noreply.github.com>
2023-05-23 15:21:16 -0500

    Update 174_FY2023.sql

--- 174_FY2023.sql
+++ 174_FY2023.sql
@@ -76,7 +76,7 @@ select
   null as Sum_of_Units____,
   null as Kg_sales,
   (sales/TC) as Sales_USD,
-  ((Standard_material + standard_labor + standard_OH )/TC) as Sum_of_costs,
+ ((COALESCE(Standard_material, 0) + COALESCE(standard_labor, 0) + COALESCE(standard_OH, 0) )/TC) as Sum_of_costs,
   (standard_material/TC) as Raw_Material_USD,
   (standard_labor/TC) as Operating_Cost__including_Direct_Labor__USD,
   (standard_OH/ TC) as Indirect_Cost__including_OH__USD,
