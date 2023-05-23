803e34535498f7a84bda42f4733a4185c6a9b3c5
Juanp-Gomez <131492820+Juanp-Gomez@users.noreply.github.com>
2023-05-23 15:59:49 -0500

    Update PPM_FY2023.sql

--- PPM_FY2023.sql
+++ PPM_FY2023.sql
@@ -80,7 +80,8 @@ group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
   null as Discount_Rebate_Amount,
   null as Return_Tagging,
   Variacion_Raw_Material,
-  Variation_X_Operation
+  Variation_X_Operation,
+  coalesce(Variacion_Raw_Material,0)+coalesce(Variation_X_Operation,0) as Total_variation
 
 from(select *,
 case when safe_cast(month as string)= '1' then 0.0504556
