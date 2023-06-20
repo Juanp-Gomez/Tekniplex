0adff4609badbdbfa4bdf738a0405836d15ada2e
Juanp-Gomez <131492820+Juanp-Gomez@users.noreply.github.com>
2023-06-20 18:22:25 -0500

    Update MD_PPC_Final.sql

--- MD_PPC_Final.sql
+++ MD_PPC_Final.sql
@@ -1,4 +1,3 @@
-
 with all_fields as(
 select distinct
 entity as entity_code,
@@ -15,7 +14,7 @@ transaction_date as t_date,
 _Month as Month,
 Customer_ID,
 Customer_name,
-Customer_location,
+Customer_location_f2 as customer_location, 
 Customer_GroupMaster_Account as cust_group,
 SKU,
 product__description as sku_description,
@@ -25,7 +24,7 @@ Product_type,
 primary_substrate_ as primary_substrate,
 end_market,
 channel,
-Intercompany_Transaction_Tagging,
+Intercompany,
 Trim(upper(Unit_of_Measurement)) as un_of_measurement,
 SUM(quantity) AS quantity,
 SUM(Sales_Amount) AS sales,
@@ -50,21 +49,32 @@ case when year = 2022 and _month = 5 then 0.0002572
       when year = 2023 and _month = 3 then 0.000215
 
       end as TC
- FROM `responsive-gist-387019.tekniplex.20230526_FY23_MD_PPC`
+from ( select *,
+case when Intercompany_Transaction_Tagging = '0' then ''
+    when Intercompany_Transaction_Tagging = '#N/A' then ''
+    when Intercompany_Transaction_Tagging = 'Interco.' then '1'
+    when Intercompany_Transaction_Tagging = 'INTERCOMPANY  ' then '1'
+    end as Intercompany,
+    first_value(customer_location) over(partition by customer_ID order by customer_location desc ) as customer_location_f2,
+    FROM `responsive-gist-387019.tekniplex.20230526_FY23_MD_PPC`
+)
+ 
  group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
  order by 1
-), 
+),
 
 final_md_ppc as(select 
 entity_code,
 entity_name,
 Plant_Business_main_line,
+year,
+month,
 FY,
 date(concat(year,'-',month,'-','01')) as t_date_f,
 t_date, -- esta fecha de factura no es con la que se cuenta en el estado financiera ya que las facturas se confirmaban cuando el cliente recogia el pedido o se confirmaba la transaccion
 Customer_ID,
 Customer_Name,
-Customer_Location,
+customer_location,
 cust_group,
 SKU,
 sku_description,
@@ -92,7 +102,7 @@ lower(product_class) as Product_Class_Homologated,
 'N/A' as Product_Type_Homologated,
 primary_substrate as Primary_Substrate_Homologated,
 end_market as End_Market_Homologated,
-Intercompany_Transaction_Tagging as intercompany,
+first_value(Intercompany) over(partition by customer_ID order by Intercompany desc ) as Intercompany,
 Un_of_Measurement as UN_of_measurement,
 round(Quantity,2) as quantity,
 null as Sum_of_units,
@@ -116,90 +126,42 @@ round ((variation_ro*tc),2) as variation_ro,
 from all_fields
 )
 
-,total_sales_product_line as(
------------------------------------------------ HOMOLOGACIONES --------------------------------------------------------------------
-
----------------------- VENTAS POR CADA UNA DE LAS LINEAS DE PRODUCTO --------
-SELECT
-    entity_name,
-    product_line,
-    Round(SUM(sales_USD),1) AS total_sales,
-    round((SUM(sales_USD) / (SELECT SUM(sales_USD) FROM final_md_ppc where t_date between '2022-07-01'and '2023-03-31'
-    and entity_name = '5250 - PPC')) *100,1) AS sales_percentage 
-FROM
-    final_md_ppc
-where t_date between '2022-07-01'and '2023-03-31'
-    and entity_name = '5250 - PPC'
-GROUP BY
-    1, product_line
-
-
-)
-
---select * from total_sales_product_line
-
-, intercompany_sales as(
- SELECT distinct
--- EXTRACT(YEAR FROM DATE(t_date)) AS year,
--- EXTRACT(MONTH FROM DATE(t_date)) AS month,
- customer_id,
- customer_name,
- customer_location, 
- intercompany, 
- round(sum(sales_usd),1) as sales,
- round((SUM(sales_USD) / (SELECT SUM(sales_USD) FROM final_md_ppc where intercompany = 'INTERCOMPANY  ' and  FY = 2023 
- and entity_name = '5250 - PPC')) *100,1)
- FROM final_md_ppc
- where intercompany = 'INTERCOMPANY  ' and  FY = 2023 
- and entity_name = '5250 - PPC'
- group by 1,2,3,4
-)
-
-, intercompany_sales_2 as(
- SELECT distinct
--- EXTRACT(YEAR FROM DATE(t_date)) AS year,
--- EXTRACT(MONTH FROM DATE(t_date)) AS month,
- int_2,
- round(sum(sales_usd),1) as sales,
- round((SUM(sales_USD) / (SELECT SUM(sales_USD) FROM final_md_ppc where FY = 2023 
- and entity_name = '5250 - PPC')) *100,1)
- FROM(
-    select
-    *,
-    case when intercompany = 'INTERCOMPANY  ' then 1
-    else 0 end as int_2
-    from final_md_ppc
- ) 
- where FY = 2023 
- and entity_name = '5250 - PPC'
- group by 1
-)
---select * from intercompany_sales_2
-
-/*, external_sales as(
- SELECT distinct 
- Customer_Location,
- round(sum(sales_usd),1) as sales,
- round((SUM(sales_USD) / (SELECT SUM(sales_USD) FROM final_md_ppc where FY = 2023 
- and entity_name = '5250 - PPC' and int_2 = 0)) *100,1)
- FROM(
-    select
-    *,
-    case when intercompany = 'INTERCOMPANY  ' then 1
-    else 0 end as int_2
-    from final_md_ppc
- ) 
- where FY = 2023 
- and entity_name = '5250 - PPC'
- and int_2=0
- group by 1
-)
+/*select * from final_md_ppc
+where month in (10, 11, 12, 1 ,2 ,3)
 */
 
---select * from final_md_ppc
+/*select Entity_name, sku, sku_description, product_class, Primary_Substrate, intercompany, 
+sum(Sales_USD_w_discount) as Sales,
+sum(Sum_of_costs_usd) as Costs, 
+sum(Raw_Material_USD) as Raw_Mat, 
+sum(Operating_Cost_including_Dl_USD) as Operating_cost,
+sum(Indirect_Cost_including_OH__USD) as Ind_Cost,
+sum(Variation_rm) as VarRM,
+sum(Variation_ro) as VarOP,
 
-select entity_name, sum(variation_rm), sum(variation_ro
-), sum(sales_USD) from final_md_ppc
-where FY = 2023 
-group by 1
 
+from final_md_ppc 
+where month in (10, 11, 12, 1 ,2 ,3)
+
+group by 1,2,3,4,5,6
+
+*/
+
+/*select entity_name, Customer_ID,
+Customer_Name,
+Customer_location,
+intercompany, 
+sum(Sales_USD_w_discount) as Sales,
+sum(Sum_of_costs_usd) as Costs, 
+sum(Raw_Material_USD) as Raw_Mat, 
+sum(Operating_Cost_including_Dl_USD) as Operating_cost,
+sum(Indirect_Cost_including_OH__USD) as Ind_Cost,
+sum(Variation_rm) as VarRM,
+sum(Variation_ro) as VarOP,
+
+
+from final_md_ppc 
+where month in (10, 11, 12, 1 ,2 ,3)
+
+group by 1,2,3,4,5
+*/
